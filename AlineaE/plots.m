922eb545e18ed4e6ac6e125e47d76cae533a09d8
mariana <mariana.galrinho@tecnico.ulisboa.pt>
2017-11-03 18:25:42 +0000

    graficos da alinea d e e

--- AlineaE/plots.m
+++ AlineaE/plots.m
@@ -1,5 +1,5 @@
 figure
-plot(PWM,vldr_adc)
+plot(PWM,vldr_adc, '.')
 a = -0.74;
 b = 1.92;
 R = 10.0;%Kohm;
@@ -7,9 +7,9 @@ Vsensor = vldr_adc*5.0/1024.0;
 r_ldr = R.*(5-Vsensor)./Vsensor;
 lux = power(r_ldr/(power(10,b)),1/a);%pôr aqui a expressao correta
 figure
-plot(PWM,lux)
+plot(PWM,lux,'.')
 
 %GUARDAR CORRESPONDENCIA LUX-PWM
-lut=[PWM';lux']
-lut=lut';
-dlmwrite('lut_semfilter.txt',lut,'delimiter','\t');
+% lut=[PWM';lux']
+% lut=lut';
+% dlmwrite('lut_semfilter.txt',lut,'delimiter','\t');
