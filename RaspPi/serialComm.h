7e2f082b4210e448d49fa4a0313a7fbabb4ee5ca
rena2henrique <renato.manao@gmail.com>
2017-12-06 23:49:17 +0000

    serial and servers almost working together

--- RaspPi/serialComm.h
+++ RaspPi/serialComm.h
@@ -1,5 +1,5 @@
-#ifndef TCP_SERVER
-#define TCP_SERVER
+#ifndef SERIAL_COMM
+#define SERIAL_COMM
 
 //Compile as:  g++ -std=c++11 async_tcp_echo_server.cpp -lpthread -lboost_system -o server
 //Run in a separate terminal, before starting client : ./server 17000
@@ -24,22 +24,14 @@ public:
 
 	~SerialComm();
 
-	void write_handler(const error_code &ec);
-
-	void timer_handler(const error_code &ec);
-
-	void read_handler(const error_code &ec);
+	void sendMessage(std::string message);
 
 private:
-	void start_read_input();
-	void handle_read_input(const boost::system::error_code& error, std::size_t length);
 
-	int counter = 0;
+	void sendMessageHandler(const boost::system::error_code& ec);
 
 	boost::asio::io_service& io;
  	boost::asio::serial_port sp;
-  	boost::asio::streambuf read_buf;
-  	steady_timer tim;
 
   	boost::asio::posix::stream_descriptor input_;
 	boost::asio::streambuf input_buffer_;
