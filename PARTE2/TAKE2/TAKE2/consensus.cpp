93210d6c0d7665da858f7fdac492abda333b3f9a
rena2henrique <renato.manao@gmail.com>
2017-12-08 20:19:08 +0000

    commited to finish this

--- PARTE2/TAKE2/TAKE2/consensus.cpp
+++ PARTE2/TAKE2/TAKE2/consensus.cpp
@@ -179,7 +179,7 @@ float Consensus::consensusIter(){
     if(i2calib->consensusFlag !=0){
      /* d2_copy[0]=i2calib->dList.get(0);
       d2_copy[1]=i2calib->dList.get(1);*/
-      if(i = 0 && myAddress == 1) {
+      if((i != 0 && myAddress == 1) || myAddress != 1) {
         char aux_string[20];
         i2calib->string_consensus.toCharArray(aux_string, sizeof(i2calib->string_consensus));
         char *token = strtok(aux_string, " ");
@@ -191,7 +191,7 @@ float Consensus::consensusIter(){
         if(token != NULL)
           strcpy(str2, token);
         d2_copy[0] = atof(str);
-        d2_copy[0] = atof(str2);
+        d2_copy[1] = atof(str2);
       }
 
         
@@ -363,6 +363,9 @@ float Consensus::consensusIter(){
       Serial.println(d_vector);
 
       i2calib->send((byte) 5, (byte) i2calib->getAddr(0), d_vector);
+
+      Serial.print("i = ");
+      Serial.println(i);
 
       i++;
 
